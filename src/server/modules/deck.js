b7ff6f168a07e1937c19a6e028d51a92041a03b0
Pavlo Evsegneev <Pavlo_Evsegneev@epam.com>
2014-09-29 18:26:45 +0300

    Add simple deck building

--- src/server/modules/deck.js
+++ src/server/modules/deck.js
@@ -1,10 +1,19 @@
 module.exports = function() {
     var deck = [];
+    var values = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+    var suites = ['S', 'T', 'D', 'H'];
 
     init();
 
     function init() {
-        deck[51] = 'lala';
+        deck = values.reduce(function(curr, item) {
+            var temp = [];
+            suites.forEach(function(suite) {
+                temp.push({'value': item, 'suite': suite});
+            });
+
+            return curr.concat(temp);
+        }, []);
     }
 
     function _getDeck() {
--- test/server/deck.tests.js
+++ test/server/deck.tests.js
@@ -1,9 +1,14 @@
 var Deck = require('../../src/server/modules/deck.js');
 var chai = require('chai');
+var _ = require('lodash');
 
 describe('Deck tests', function() {
 
     var deck;
+
+    var rawDeck = [
+        {"value":"A","suite":"S"},{"value":"A","suite":"T"},{"value":"A","suite":"D"},{"value":"A","suite":"H"},{"value":"2","suite":"S"},{"value":"2","suite":"T"},{"value":"2","suite":"D"},{"value":"2","suite":"H"},{"value":"3","suite":"S"},{"value":"3","suite":"T"},{"value":"3","suite":"D"},{"value":"3","suite":"H"},{"value":"4","suite":"S"},{"value":"4","suite":"T"},{"value":"4","suite":"D"},{"value":"4","suite":"H"},{"value":"5","suite":"S"},{"value":"5","suite":"T"},{"value":"5","suite":"D"},{"value":"5","suite":"H"},{"value":"6","suite":"S"},{"value":"6","suite":"T"},{"value":"6","suite":"D"},{"value":"6","suite":"H"},{"value":"7","suite":"S"},{"value":"7","suite":"T"},{"value":"7","suite":"D"},{"value":"7","suite":"H"},{"value":"8","suite":"S"},{"value":"8","suite":"T"},{"value":"8","suite":"D"},{"value":"8","suite":"H"},{"value":"9","suite":"S"},{"value":"9","suite":"T"},{"value":"9","suite":"D"},{"value":"9","suite":"H"},{"value":"10","suite":"S"},{"value":"10","suite":"T"},{"value":"10","suite":"D"},{"value":"10","suite":"H"},{"value":"J","suite":"S"},{"value":"J","suite":"T"},{"value":"J","suite":"D"},{"value":"J","suite":"H"},{"value":"Q","suite":"S"},{"value":"Q","suite":"T"},{"value":"Q","suite":"D"},{"value":"Q","suite":"H"},{"value":"K","suite":"S"},{"value":"K","suite":"T"},{"value":"K","suite":"D"},{"value":"K","suite":"H"}
+    ];
 
     beforeEach(function() {
         deck = new Deck();
@@ -11,6 +16,7 @@ describe('Deck tests', function() {
 
     it('should build new deck', function() {
         chai.assert.equal(deck.getDeck().length, 52, 'deck contains 52 cards after init');
+        chai.assert(_.isEqual(deck.getDeck(), rawDeck), 'deck equals sorted deck');
     });
 
 });
