6b95613c2c9b67ea0d79ae400809609248ff56c6
Pavlo Evsegneev <Pavlo_Evsegneev@epam.com>
2014-09-30 12:25:26 +0300

    Replace Chrome with firefox because Travis CI supports firefox only

--- karma.conf.js
+++ karma.conf.js
@@ -23,7 +23,7 @@ module.exports = function(config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome'],
+        browsers: ['Firefox'],
         captureTimeout: 60000,
         singleRun: true
     });
