bb567d0792be57458d5725028afbd4330457cefe
Pavlo Evsegneev <Pavlo_Evsegneev@epam.com>
2014-10-06 17:27:16 +0300

    Add facebook local tokens

--- src/server/config.js
+++ src/server/config.js
@@ -1,22 +1,22 @@
 var config = {
     oauth: {
         facebook: {
-            clientID: 'get_your_own',
-            clientSecret: 'get_your_own',
-            callbackURL: 'http://127.0.0.1:1337/auth/facebook/callback'
+            clientID: '1473170869614770',
+            clientSecret: '4e5a8216854bb3ed0d2546e647cac37e',
+            callbackURL: 'http://local.foo.com:8080/auth/facebook/callback'
         },
         twitter: {
             consumerKey: 'get_your_own',
             consumerSecret: 'get_your_own',
-            callbackURL: "http://127.0.0.1:1337/auth/twitter/callback"
+            callbackURL: "http://127.0.0.1/auth/twitter/callback"
         },
         github: {
             clientID: 'get_your_own',
             clientSecret: 'get_your_own',
-            callbackURL: "http://127.0.0.1:1337/auth/github/callback"
+            callbackURL: "http://127.0.0.1/auth/github/callback"
         },
         google: {
-            returnURL: 'http://127.0.0.1:1337/auth/google/callback',
+            returnURL: 'http://127.0.0.1/auth/google/callback',
             realm: 'http://127.0.0.1:1337'
         }
     }
